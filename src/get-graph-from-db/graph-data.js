a3960c2ebf873bc3dc9e7a48a9046b4259449412
Meisami Amir Hossein <Ah_Meisami@iranet.net>
2020-10-30 15:52:20 +0330

    add getData REST API

---
+++ src/get-graph-from-db/graph-data.js
@@ -0,0 +1,51 @@
+const oracledb = require('oracledb');
+
+function getData() {
+  return new Promise(function (resolve, reject) {
+    let conn; // Declared here for scoping purposes.
+
+    oracledb
+      .getConnection()
+      .then(function (c) {
+        // console.log('Connected to database');
+
+        conn = c;
+
+        return conn.execute(
+          `select acct_src accNo, count(*) No from atm.graph_data group by acct_src having count(*)>10 order by count(*) desc`
+          ,
+          [],
+          {
+            outFormat: oracledb.OBJECT
+          }
+        );
+      })
+      .then(
+        function (result) {
+          // console.log('Query executed');
+
+          resolve(result);
+        },
+        function (err) {
+          console.log('Error occurred', err);
+
+          reject(err);
+        }
+      )
+      .then(function () {
+        if (conn) {
+          // If conn assignment worked, need to close.
+          return conn.close();
+        }
+      })
+      .then(function () {
+        console.log('Connection closed');
+      })
+      .catch(function (err) {
+        // If error during close, just log.
+        console.log('Error closing connection', err);
+      });
+  });
+}
+
+module.exports.getData = getData;
--- src/get-graph-from-db/index.js
+++ src/get-graph-from-db/index.js
@@ -1,12 +1,29 @@
 const oracledb = require('oracledb');
 const dbConfig = require('./db-config.js');
-const graphNode = require('./graph-node.js');
+const graphData = require('./graph-data.js');
 const graphEdge = require('./graph-edge.js');
+const graphNode = require('./graph-node.js');
 
 var express = require('express');
 var app = express();
 
 app.use(express.json());
+
+app.get('/getData', function (req, res, next) {
+  oracledb.createPool(dbConfig)
+  .then(function() {
+    return graphData.getData();
+  })
+  .then(function(result) {
+    res.json(result);
+    console.log('result.metaData',result.metaData);
+    console.log('result.rows',result.rows);
+    console.log('result.rows.length',Object.keys(result.rows).length);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+});
 
 app.get('/getNode', function (req, res, next) {
   let accNo = req.query.accNo;
