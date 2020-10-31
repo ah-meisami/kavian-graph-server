cb7f8f42e9962fad54e75199f4e1e3b602019f50
Meisami Amir Hossein <Ah_Meisami@iranet.net>
2020-10-31 11:32:41 +0330

    change return json format to be acceptable by vis-network

--- src/get-graph-from-db/index.js
+++ src/get-graph-from-db/index.js
@@ -30,15 +30,21 @@ app.get('/getData', function (req, res, next) {
 app.get('/getNode', function (req, res, next) {
   let accNo = req.query.accNo;
 
+  let new_nodes_list = []; //this new nodes list hsa a format that is acceptable by vis-network lib for graph visualization.
+
   oracledb.createPool(dbConfig)
   .then(function() {
     return graphNode.getNode(accNo);
   })
   .then(function(result) {
-    res.json(result);
-    console.log('result.metaData',result.metaData);
-    console.log('result.rows',result.rows);
-    console.log('result.rows.length',Object.keys(result.rows).length);
+    result.rows.map(row => {
+      let new_node = {id: row.ID, label: row.NODE};
+      new_nodes_list.push(new_node);
+    });
+    res.json(new_nodes_list);
+    // console.log('result.metaData',result.metaData);
+    // console.log('result.rows',result.rows);
+    // console.log('result.rows.length',Object.keys(result.rows).length);
   })
   .catch(function(err) {
     console.log(err);
@@ -48,22 +54,37 @@ app.get('/getNode', function (req, res, next) {
 app.get('/getEdge', function (req, res, next) {
   let accNo = req.query.accNo;
 
+  let new_edges_list = []; //this new edges list hsa a format that is acceptable by vis-network lib for graph visualization.
+
   oracledb.createPool(dbConfig)
   .then(function() {
     return graphEdge.getEdge(accNo);
   })
   .then(function(result) {
-    res.json(result);
-    console.log('result.metaData',result.metaData);
-    console.log('result.rows',result.rows);
-    console.log('result.rows.length',Object.keys(result.rows).length);
+    result.rows.map(row => {
+      let new_edge = {
+        edge_id: row.EDGE_ID,
+        node_from_id: row.NODE_FROM_ID,
+        node_to_id: row.NODE_TO_ID,
+        node_from_acct: row.NODE_FROM_ACCT,
+        node_to_acct: row.NODE_TO_ACCT,
+        edge_attr_trn_cd: row.EDGE_ATTR_TRN_CD,
+        edge_attr_trn_date: row.EDGE_ATTR_TRN_DATE,
+        edge_attr_trn_amnt: row.EDGE_ATTR_TRN_AMNT
+      };
+      new_edges_list.push(new_edge);
+    });
+    res.json(new_edges_list);
+    // console.log('result.metaData',result.metaData);
+    // console.log('result.rows',result.rows);
+    // console.log('result.rows.length',Object.keys(result.rows).length);
   })
   .catch(function(err) {
     console.log(err);
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30;
 app.listen(PORT, function () {
   console.log(`web server listening on port ${PORT}`)
 });
