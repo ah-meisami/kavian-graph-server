c0d2ddbc12fcc64113708732c245372abc97f315
Meisami Amir Hossein <Ah_Meisami@iranet.net>
2020-11-02 14:15:52 +0330

    read important data from system environment variables

--- src/get-graph-from-db/db-config.js
+++ src/get-graph-from-db/db-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  user: 'atm',
-  password: 'atm',
-  connectString: '192.168.234.130:1522/ORCL',
+  user: process.env.NODE_ORACLEDB_USER || 'atm',
+  password: process.env.NODE_ORACLEDB_PASSWORD || 'atm',
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || '192.168.234.130:1522/ORCL',
   poolMin: 10,
   poolMax: 100,
   poolIncrement: 5,
